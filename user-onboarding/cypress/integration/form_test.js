c745a1143af98b218c66e7f0b72c23bccacdbb1c
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-01-21 18:05:54 -0500

    Simulated User Action to test checking terms of service and selecting a role from dropdown

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -8,10 +8,14 @@ describe('Forms App', () => {
         expect(2 + 2).not.to.equal(5)
     })
 
-    it('make sure we can input a name', () => {
-        const nameInputField = () => cy.get(':nth-child(1) > input')
-        const name = 'Jayaram Sivaraman Nair'
+    const nameInputField = () => cy.get(':nth-child(1) > input')
+    const emailInput = () => cy.get(':nth-child(2) > input')
+    const passwordInput = () => cy.get(':nth-child(3) > input')
+    const checkBox = () => cy.get(':nth-child(4) > input')
+    const dropDown = () => cy.get('select')
 
+    it('make sure we can input a name', () => {
+        const name = 'Jayaram Sivaraman Nair'
         nameInputField().type(name).should('have.value', name)
     })
 
@@ -20,11 +24,18 @@ describe('Forms App', () => {
     })
 
     it('make sure that e-mail address and password can be input', () => {
-        const emailInput = () => cy.get(':nth-child(2) > input')
         const emailAddress = 'jsnair46@gmail.com'
         emailInput().type(emailAddress).clear()
-
-        const passwordInput = () => cy.get(':nth-child(3) > input')
         passwordInput().type('Jayaram123').clear()
+    })
+
+    it('make sure that the terms of service can be checked', () => {
+        checkBox().check().should('be.checked')
+        checkBox().uncheck().should('not.be.checked')
+    })
+
+    it('make sure to check that a role can selected from dropdown', () => {
+        dropDown().select('Software Engineer').should('have.value', '3')
+        dropDown().select('--Select One--')
     })
 })
