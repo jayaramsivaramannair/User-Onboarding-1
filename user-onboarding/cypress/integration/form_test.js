34112e7f68fff9f9866b8680b16cfb842250645a
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-01-21 06:17:11 -0500

    Acquired the name input field, typed a name into it and checked if the field contains the entered text

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -7,4 +7,12 @@ describe('Forms App', () => {
         expect(2 + 2).to.equal(4)
         expect(2 + 2).not.to.equal(5)
     })
+
+    it('make sure we can input a name', () => {
+        const nameInputField = () => cy.get(':nth-child(1) > input')
+        const name = 'Jayaram Sivaraman Nair'
+
+        nameInputField().clear().type(name)
+        nameInputField().should('have.value', name)
+    })
 })
